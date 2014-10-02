4d9d485e782b358ba3a78637226a208aef8b40ea
zmuzik <zbynek.muzik@gmail.com>
2014-10-02 17:32:15 +0900

    disable list-editing dialog

---
+++ app/src/main/java/zmuzik/czechstocks/EditStockListActivity.java
@@ -0,0 +1,15 @@
+package zmuzik.czechstocks;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+
+public class EditStockListActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_stock_list);
+    }
+
+}
