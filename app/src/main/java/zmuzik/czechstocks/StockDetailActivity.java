e4e45f136c06ae831fce4f06a1cd7578d91a040e
zmuzik <zbynek.muzik@gmail.com>
2014-10-06 20:03:24 +0900

    polishing of the previous

--- app/src/main/java/zmuzik/czechstocks/StockDetailActivity.java
+++ app/src/main/java/zmuzik/czechstocks/StockDetailActivity.java
@@ -72,17 +72,13 @@ public class StockDetailActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
     public static class PlaceholderFragment extends Fragment {
 
         public PlaceholderFragment() {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_stock_basic_info, container, false);
             return rootView;
         }
