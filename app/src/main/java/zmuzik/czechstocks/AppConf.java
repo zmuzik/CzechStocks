ac5e5c8a52d0407436ecd059a4856ad3d6d33a19
zmuzik <zbynek.muzik@gmail.com>
2014-10-08 17:47:41 +0900

    redirected api to sandbox

--- app/src/main/java/zmuzik/czechstocks/AppConf.java
+++ app/src/main/java/zmuzik/czechstocks/AppConf.java
@@ -2,6 +2,6 @@ package zmuzik.czechstocks;
 
 public class AppConf {
 
-    public static final String SERVER_API_ROOT = "http://127.0.0.1:5000";
+    public static final String SERVER_API_ROOT = "http://168.235.154.199";
 
 }
