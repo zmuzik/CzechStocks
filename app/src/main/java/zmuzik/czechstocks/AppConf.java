f13f315ed9fca588c06af8c09012fce7ee83c4f9
zmuzik <zbynek.muzik@gmail.com>
2014-11-05 13:50:24 +0900

    price graph pivot point

--- app/src/main/java/zmuzik/czechstocks/AppConf.java
+++ app/src/main/java/zmuzik/czechstocks/AppConf.java
@@ -4,4 +4,5 @@ public class AppConf {
 
     public static final String SERVER_API_ROOT = "http://168.235.154.199";
     public static final String DB_NAME = "czech-stocks-db";
+    public static final String EXCHANGE_TIME_ZONE = "Europe/Prague";
 }
--- app/src/main/java/zmuzik/czechstocks/activities/StockDetailActivity.java
+++ app/src/main/java/zmuzik/czechstocks/activities/StockDetailActivity.java
@@ -2,20 +2,35 @@ package zmuzik.czechstocks.activities;
 
 import android.app.Activity;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.androidplot.xy.LineAndPointFormatter;
+import com.androidplot.xy.SimpleXYSeries;
+import com.androidplot.xy.XYPlot;
 import com.crashlytics.android.Crashlytics;
 
+import java.text.FieldPosition;
+import java.text.Format;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.TimeZone;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import de.greenrobot.dao.query.QueryBuilder;
 import de.greenrobot.dao.query.WhereCondition;
 import zmuzik.czechstocks.App;
+import zmuzik.czechstocks.AppConf;
 import zmuzik.czechstocks.R;
 import zmuzik.czechstocks.Utils;
 import zmuzik.czechstocks.adapters.DividendListAdapter;
@@ -24,6 +39,7 @@ import zmuzik.czechstocks.dao.Dividend;
 import zmuzik.czechstocks.dao.Stock;
 import zmuzik.czechstocks.dao.StockDao;
 import zmuzik.czechstocks.dao.StockInfo;
+import zmuzik.czechstocks.dao.TodaysQuote;
 
 
 public class StockDetailActivity extends Activity {
@@ -41,6 +57,9 @@ public class StockDetailActivity extends Activity {
     TextView pe;
     @InjectView(R.id.dividendsListView)
     ListView dividendsListView;
+
+    @InjectView(R.id.stockGraph)
+    XYPlot stockGraph;
 
 
     @Override
@@ -62,6 +81,7 @@ public class StockDetailActivity extends Activity {
         loadStockFromDb();
         updateBasicInfo();
         updateDividendsList();
+        updateGraph();
     }
 
     private void loadStockFromDb() {
@@ -91,13 +111,57 @@ public class StockDetailActivity extends Activity {
     }
 
     private void updateDividendsList() {
-        //List<Dividend> dividends = mStock.getDividendList();
-
         QueryBuilder qb = App.get().getDaoSession().getDividendDao().queryBuilder();
         qb.where(new WhereCondition.StringCondition("ISIN = '" + mStock.getIsin()
                 + "' ORDER BY PAYMENT_DATE COLLATE LOCALIZED DESC"));
         List<Dividend> dividends = qb.list();
-
         dividendsListView.setAdapter(new DividendListAdapter(this, dividends));
+    }
+
+    private void updateGraph() {
+        // prepare data
+        List<TodaysQuote> quoteList = mStock.getTodaysQuoteList();
+        ArrayList<Number> prices = new ArrayList<Number>();
+        ArrayList<Number> dates = new ArrayList<Number>();
+        for (TodaysQuote todaysQuote : quoteList) {
+            prices.add(todaysQuote.getPrice());
+            dates.add(todaysQuote.getStamp().getTime());
+        }
+        SimpleXYSeries priceSeries = new SimpleXYSeries(dates, prices, null);
+
+        //graph formatting
+        LineAndPointFormatter priceFormat = new LineAndPointFormatter(
+                Color.rgb(0, 0, 100),           // line color
+                null,                           // point color
+                Color.rgb(0, 0, 200), null);    // fill color
+        Paint lineFill = new Paint();
+        lineFill.setAlpha(200);
+        lineFill.setShader(new LinearGradient(0, 0, 0, 250, Color.WHITE, Color.GREEN, Shader.TileMode.MIRROR));
+
+        stockGraph.setDomainValueFormat(new HourlyDateFormat());
+        stockGraph.addSeries(priceSeries, priceFormat);
+        stockGraph.getLegendWidget().setVisible(false);
+    }
+
+
+    private class HourlyDateFormat extends Format {
+        SimpleDateFormat dateFormat;
+
+        public HourlyDateFormat() {
+            TimeZone exchangeTimeZone = TimeZone.getTimeZone(AppConf.EXCHANGE_TIME_ZONE);
+            dateFormat = new SimpleDateFormat("HH:mm");
+            dateFormat.setTimeZone(exchangeTimeZone);
+        }
+
+        @Override
+        public StringBuffer format(Object obj, StringBuffer toAppendTo, FieldPosition pos) {
+            Date date = new Date(((Number) obj).longValue());
+            return dateFormat.format(date, toAppendTo, pos);
+        }
+
+        @Override
+        public Object parseObject(String source, ParsePosition pos) {
+            return null;
+        }
     }
 }
