000ffd37c2838020ba5e0228cf5b542ebbccd6e5
zmuzik <zbynek.muzik@gmail.com>
2014-12-23 21:00:39 +0100

    pivot

---
+++ api/export_for_build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sqlite3 data.db 'select * from dividend;' > ../app/src/main/assets/dividend.csv
+sqlite3 data.db 'select * from historical_quote;' > ../app/src/main/assets/historical_quote.csv
+sqlite3 data.db 'select * from todays_quote;' > ../app/src/main/assets/todays_quote.csv
+sqlite3 data.db 'select * from stock_detail;' > ../app/src/main/assets/stock_detail.csv
+
