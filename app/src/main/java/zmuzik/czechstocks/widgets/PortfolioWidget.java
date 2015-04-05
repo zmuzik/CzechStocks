5cc1a74e4d92a6a50b59bc9dd91e7ad8d111171c
zmuzik <zbynek.muzik@gmail.com>
2015-04-05 15:47:29 +0800

    portfolio widget - phase 1

---
+++ app/src/main/java/zmuzik/czechstocks/widgets/PortfolioWidget.java
@@ -0,0 +1,45 @@
+package zmuzik.czechstocks.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import zmuzik.czechstocks.R;
+
+public class PortfolioWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        for (int i = 0; i < appWidgetIds.length; i++) {
+            PortfolioWidgetConfigureActivity.deleteTitlePref(context, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.portfolio_widget);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
+
+
